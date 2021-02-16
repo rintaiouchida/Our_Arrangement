191d5fdcc2a24ceed363cd3a0f00556c926a4357
rintaro uchida <v795b3eed7qzaE05SE@softbank.ne.jp>
2021-02-16 09:42:06 +0900

    change_usertable_and_register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
 
 class RegisterController extends Controller
 {
@@ -52,6 +53,8 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'picture'=>['max:255','file','mimes:jpeg,png'],
+            'birthday'=>['required','date'],
         ]);
     }
 
@@ -63,10 +66,15 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        $image=$data['picture'];
+        $path=Storage::disk('s3')->putFile('/test', $data['picture'], 'public');
+
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'picture'=>Storage::disk('s3')->url($path),
+            'birthday'=>$data['birthday'],
         ]);
     }
 }
