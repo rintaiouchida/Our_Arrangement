8a36504dc04b94999a8d414e5aeac2867dbe3eb2
rintaro uchida <v795b3eed7qzaE05SE@softbank.ne.jp>
2021-03-08 14:07:17 +0900

    fix:シードの修正

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -16,14 +16,29 @@ class PostsTableSeeder extends Seeder
 
        
         DB::table('posts')->insert([
-            'name'=>'野獣先輩',
+            ['name'=>'野獣先輩',
             'user_id'=>3,
             'genre_id'=>3,
             'material'=>'豆腐',
             'icon_picture'=>'https://uchidamyfirst.s3.ap-northeast-1.amazonaws.com/test/ktJGBUthH6GooHekRJoq0iq4PrbLfo9LHdiUvuvb.jpg',
             'arrange'.'_origin'=>'豆腐',
-            
+            'created_at'=>'1998-03-11',
+            ],
+            [
+            'name'=>'ペヤングコロッケ',
+            'user_id'=>2,
+            'genre_id'=>3,
+            'material'=>'・ペヤング
+            ・油
+            ・卵
+            ・パン粉',
+            'icon_picture'=>'https://uchidamyfirst.s3.ap-northeast-1.amazonaws.com/test/9uCMvUI81I1qAPR1iPZRyJRsGqkdN5AQihPCf0IC.png',
+            'arrange'.'_origin'=>'ペヤング',
+            'created_at'=>'1998-03-11',
+
+            ],
         ]);
+        
         
     }
 }
