18071061e2d9cf6f9d14e4e44ba09a6fcadeecb1
rintaro uchida <v795b3eed7qzaE05SE@softbank.ne.jp>
2021-03-08 13:51:54 +0900

    fix:viewの修正

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,188 +6,7 @@
   <title>Document</title>
   <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-  <style>
-    
-    .container{
-      margin:0;
-      background-color:#e2dbc6;
-      text-align:center;
-      width:100vw;
-      
-    }
-    .btn{
-      position:relative;
-    }
-    .btn>button{
-      position:fixed;
-      top:0;
-      left:0;
-      z-index:1;
-    }
-    button > span{
-      background-color:black;
-      width:30px;
-      height:5px;
-      display:block;
-      margin-bottom:5px;
-    }
-    .btn2{
-      position:relative;
-    }
-
-    .btn2>a{
-      position:fixed;
-      top:0;
-      right:0;
-      z-index:1;
-    }
-
-    .main{
-      /* background-color:pink; */
-      margin-top:100px;
-    
-    }
-
-    .box{
-      height:500px;
-      display:inline-block;
-      margin-bottom:50px;
-      position:relative;
-    }
-
-    .box>img{
-      border:5px solid white;
-    }
-   
-
-    .title{
-      position:absolute;
-      left:20px;
-      right:20px;
-      bottom:5px;
-      color:red;
-      background-color:yellow;
-     
-      font-size:30px;
-      opacity:0.8;
-    }
-
-    .clear{
-      clear:both;
-    }
-
-   /* メイン画面②の設計 */
-    .container2{
-      background-color:#f0ece8;
-      height:100vh;
-      width:60vw;
-      position:fixed;
-      top:0;
-      left:-60vw;
-      
-    }
-    header{
-      height:7%;
-    }
-
-    .myface{
-      height:23%;
-      /* background-color:red; */
-      width:20%;
-      float:left;
-    }
-    .myinfo{
-      width:80%;
-      height:23%;
-      float:left;
-    }
-    .myname{
-      /* background-color:blue; */
-      width:100%;
-      height:50%;
-      text-align:center;
-      line-height:11.5vh;
-      font-size:40px;
-    }
-    .myff{
-       /* background-color:red; / */
-      width:85%;
-      height:50%;
-      float:right;
-    }
-    .follow{
-      /* background-color:red; */
-      float:left;
-      width:50%;
-      height:100%;
-      text-align:center;
-      font-size:25px;
-    }
-    .follow >a:hover{
-      color:pink;
-    }
-    .followed{
-      /* background-color:red; */
-      float:left;
-      width:50%;
-      height:100%;
-      text-align:center;
-      font-size:25px;
-    }
-
-    .post{
-      height:14%;
-    }
-    .post:active{
-      color:red;
-    }
-    .search{
-      height:14%;
-      
-    }
-    .rank{
-      height:14%;
-      
-    }
-    .delete{
-      height:14%;
-      
-    }
-    .logout{
-      height:14%;
-    }
-
-    /* ここまで */
-
-    /* メイン画面②のボタンの要素 */
-    .info-btn{
-      display:block;
-      text-decoration:none; 
-      line-height:14vh;
-      background-color:#f0ece8;
-      border-top:5px solid #89c3eb;
-      color:#89c3eb;
-      text-shadow:3px 3px white;
-    }
-    .info-btn:hover{
-      background-color:white;
-    }
-    .info-btn:active{
-      color:red;
-    }
-    
-
-    /* メイン画面②がスライドしてくる */
-    .active{
-      transform:translateX(60vw);
-      transition:1s;
-    }
-    .nonactive{
-      transform:translateX(0);
-      transition:1s;
-    }
-    
-  </style>
+  <link href="{{ asset('css/main.css') }}" rel="stylesheet">
 </head>
 <body>
 <!-- style="margin:0; position:relative;" -->
@@ -206,11 +25,12 @@
     <div class="main col-md-12">
       @if(!empty($posts))
       @foreach($posts as $post)
-      <a href="menu/{{$post->id}}" class="box col-md-5 ">
+      <a href="/menu/{{$post->id}}" class="box col-md-5 ">
       <img src="{{$post->icon_picture}}" style="width:100%; height:100%;display:block;"></img>
       <div class="title">タイトル:{{$post->name}}<br>作成日時:{{$post->created_at}}</div>
       
       </a>
+      
       @endforeach
       @endif
       
