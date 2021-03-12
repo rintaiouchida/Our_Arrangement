6697abdf88d89be79f1feb66026deb31bc72d15a
rintaro uchida <v795b3eed7qzaE05SE@softbank.ne.jp>
2021-03-12 17:44:34 +0900

    fix:seedの修正

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             PostsTableSeeder::class,
             StepsTableSeeder::class,
             GenresTableSeeder::class,
+            LikesTableSeeder::class,
         ]);
     }
 }
