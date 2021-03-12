288b6201c2ba95553c6e5bcc62f583065b94ebf7
rintaro uchida <v795b3eed7qzaE05SE@softbank.ne.jp>
2021-03-12 17:17:43 +0900

    feat:新しいview生成

---
+++ resources/views/search_rank.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+  <div class="container col-md-12" style="background-color:yellow;">
+    <a class="btn btn-primary col-md-3" href="/search_rank_genre">ジャンル別</a>
+    <a class="btn btn-primary col-md-3" href="/search_rank_age">年齢別</a>
+  </div>
+</body>
+</html>
