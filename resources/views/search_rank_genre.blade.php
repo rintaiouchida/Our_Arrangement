2909848cd2e9fcc126da9ca7586ff4e350812e0d
rintaro uchida <v795b3eed7qzaE05SE@softbank.ne.jp>
2021-03-12 17:19:08 +0900

    feat:新しいview生成

---
+++ resources/views/search_rank_genre.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+  <div class="container col-md-12" style="background-color:yellow;">
+    <a href="/search_rank_genre/1" class="btn btn-primary offset-md-1">ご飯もの</a>
+    <a href="/search_rank_genre/2" class="btn btn-primary offset-md-1">麺類</a>
+    <a href="/search_rank_genre/3" class="btn btn-primary offset-md-1">揚げ物</a>
+    <a href="/search_rank_genre/4" class="btn btn-primary offset-md-1">スープ</a>
+    <a href="/search_rank_genre/5" class="btn btn-primary offset-md-1">デザート</a>
+    <div><a href="/main" class="btn btn-danger offset-md-1">戻る</a></div>
+  </div>
+</body>
+</html>
